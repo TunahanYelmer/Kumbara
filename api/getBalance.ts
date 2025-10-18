1022be6286b0987d75b9317dc7345cf0a28d8961
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-10-19 00:44:02 +0300

    adjusted api adress

--- api/getBalance.ts
+++ api/getBalance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function getBalance(): Promise<number> {
-  const response = await axios.get("http://192.168.1.109:8080/balance");
+  const response = await axios.get("http://192.168.1.108:8080/balance");
   return response.data.balance;
 }
