a38a1d60857ac1a71015b2bbd5e73be14f4e7b64
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-10-19 00:45:29 +0300

    adjusted api adress

--- api/postBalance.ts
+++ api/postBalance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export async function postBalance(balance: number): Promise<void> {
-  await axios.post("http://192.168.1.109:8080/balance", { balance });
+  await axios.post("http://192.168.1.108:8080/balance", { balance });
 }
