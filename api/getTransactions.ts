045789e91d438c5b0d60abace8e95908ef00a60b
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-10-19 00:44:24 +0300

    adjusted api adress

--- api/getTransactions.ts
+++ api/getTransactions.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { Transactions } from "../context/reducer";
 
-export async function getBalance(): Promise<number> {
-  const response = await axios.get("http://192.168.1.109:8080/transactions");
-  return response.data.balance;
+export async function getTransactions(): Promise<Transactions[]> {
+  const response = await axios.get("http://192.168.1.108:8080/transactions");
+  return response.data;
 }
