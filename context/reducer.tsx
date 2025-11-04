f9980a1f93976eebc17002158ff513d25707df71
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-11-04 14:04:11 +0300

    feat: added new dispatch actions for setting screen toggles

--- context/reducer.tsx
+++ context/reducer.tsx
@@ -7,20 +7,51 @@ export interface Transactions {
 }
 
 export interface State {
+  DarkMode: boolean;
+  BioEnabled: boolean;
+  PinEnabled: boolean;
   Balance: number;
   Transactions: Transactions[];
+  Currency?: Currency[];
   // Add more state properties if needed
+}
+export interface Currency {
+  code: string;
+  symbol: string;
+  name: string;
+}
+export interface NotificationSettings {
+  dailyReminder: boolean;
+  goalAlerts: boolean;
+}
+export interface ThemeSettings {
+  darkMode: boolean;
+}
+export interface SecuritySettings {
+  pinEnabled: boolean;
+  biometricEnabled: boolean;
 }
 
 export type Action =
   | { type: "SET_BALANCE"; Balance?: number }
-  | { type: "SET_TRANSACTIONS"; Transactions?: Transactions[] };
+  | {
+      type: "SET_TRANSACTIONS";
+      Transactions?: Transactions[];
+    }
+  | { type: "SET_CURRENCY"; Currency?: Currency[] }
+  | { type: "SET_PIN_SECURİTY" }
+  | { type: "SET_BIO_SECURITY" }
+  | { type: "SET_DARK_MODE" };
 
 // Add more action types if needed
 
 export const initialState: State = {
+  DarkMode: false,
+  BioEnabled: false,
+  PinEnabled: false,
   Balance: 0,
-  Transactions: []
+  Transactions: [],
+  Currency: [{ code: "TRY", symbol: "₺", name: "Turkish Lira" }]
   // Add more state properties if needed
 };
 
@@ -35,6 +66,26 @@ const reducer = (state: State = initialState, action: Action): State => {
       return {
         ...state,
         Transactions: action.Transactions ?? state.Transactions
+      };
+    case "SET_CURRENCY":
+      return {
+        ...state,
+        Currency: action.Currency ?? state.Currency
+      };
+    case "SET_PIN_SECURİTY":
+      return {
+        ...state,
+        PinEnabled: !state.PinEnabled
+      };
+    case "SET_BIO_SECURITY":
+      return {
+        ...state,
+        BioEnabled: !state.BioEnabled
+      };
+    case "SET_DARK_MODE":
+      return {
+        ...state,
+        DarkMode: !state.DarkMode
       };
     // Add more cases for other actions if needed
     default:
