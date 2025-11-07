4c0bfc0abaa6768e6d48785ef8acf42c3e5f7e24
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-11-07 15:53:06 +0300

    feat: switched to theme context to allow dark and light modes and applied its color tags

--- _tabs_/home/homeScreen.tsx
+++ _tabs_/home/homeScreen.tsx
@@ -5,6 +5,7 @@ import User from '@components/User';
 import BalanceCard from '@components/BalanceCard';
 import Transactions from '@components/Transactions';
 import TransactionHistory from '@components/TransactionHistory';
+import {useTheme} from '@/context/theme/ThemeProvider';
 
 
 
@@ -12,9 +13,55 @@ import TransactionHistory from '@components/TransactionHistory';
 
 
 export default function HomeScreen() {
+  const [ theme ] = useTheme();
+  const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: theme.HomeScreenBgColor
+  },
+  container: {
+  },
+  settingsText: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 50
+  }
+  ,
+ settingsTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    padding: 16,
+    textAlign: "center"
+  },
+  settingGroup: {
+    backgroundColor: theme.HomeScreenGroupBackgroundColor,
+    marginVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8
+  },
+  groupTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    paddingVertical: 12,
+    color: theme.HomeScreenGroupTitleColor
+  },
+  settingItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 12,
+    borderTopWidth: 1,
+    borderTopColor: theme.HomeScreenItemBorderColor
+  },
+  settingValue: {
+    color: "#666"
+  }
+});
+
+
   return (
     <SafeAreaView style={styles.safeArea} edges={["top", "bottom"]}>
-      <RNStatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <RNStatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <View style={styles.container}>
         <User />
         <BalanceCard />
