8f8568f488a0162269703adf56714c243dd1179e
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-12-01 21:29:19 +0300

    routing adjustments

--- navigation/RootStack.tsx
+++ navigation/RootStack.tsx
@@ -6,13 +6,19 @@ import TabNavigator from "./TabNavigator";
 import NotificationScreen from "@/screens/notifications/NotificationScreen";
 
 import { RootStackParamList } from "./NavigationTypes";
+import HomeScreen from "@/_tabs_/home/HomeScreen";
+import SettingsScreen from "@/_tabs_/settings/SettingsScreen";
+import AuthScreen from "@/screens/auth/AuthScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Tabs" component={TabNavigator} />
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, animation: "ios_from_right" }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
       <Stack.Screen name="Notifications" component={NotificationScreen} />
     </Stack.Navigator>
   );
