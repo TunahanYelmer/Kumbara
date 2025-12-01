4bac9fdbc8921e7930a2bd17481863eeb4e87e35
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-12-01 21:25:17 +0300

    user component is separeted from the home screen and added safe area view to  main app

--- App.tsx
+++ App.tsx
@@ -6,19 +6,31 @@ import { StateProvider } from "@context/state/StateProvider";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationProvider } from "./context/navigation/NavigationProvider";
 import { navigationRef } from "./navigation/NavigationTypes";
+import Navbar from "@navigation/Navbar/Navbar";
+import User from "@components/User/User";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <StateProvider>
-        <NavigationProvider>
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <NavigationContainer ref={navigationRef}>
-              <RootStack />
-            </NavigationContainer>
-          </GestureHandlerRootView>
-        </NavigationProvider>
-      </StateProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <StateProvider>
+          <NavigationProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <SafeAreaView
+                style={{ flex: 1 }}
+                edges={["top", "left", "right"]}
+              >
+                <NavigationContainer ref={navigationRef}>
+                  <User />
+                  <RootStack />
+                  <Navbar />
+                </NavigationContainer>
+              </SafeAreaView>
+            </GestureHandlerRootView>
+          </NavigationProvider>
+        </StateProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
