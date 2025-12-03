c767bf9a77160e81f15f50808b808696e4173e3f
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-12-03 16:14:00 +0300

    corrected  api call  config

--- api/postTransactions.ts
+++ api/postTransactions.ts
@@ -3,20 +3,26 @@ import axios from "axios";
 const BASE_URL = "http://192.168.1.108:8082";
 
 export async function postTransaction(
+  jwtToken: string,
   type: "deposit" | "withdraw",
   amount: number,
-  category?: string,
-  jwtToken: string
+  category?: string
 ): Promise<void> {
   try {
-    await axios.post(`${BASE_URL}/transactions`, {
-      type,
-      amount,
-      category: category || null,
-      headers: {
-        Authorization: `Bearer ${jwtToken}`
+    await axios.post(
+      `${BASE_URL}/transactions`,
+      {
+        type,
+        amount,
+        category: category || null
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+          "Content-Type": "application/json"
+        }
       }
-    });
+    );
   } catch (err) {
     if (axios.isAxiosError(err)) {
       console.error(
