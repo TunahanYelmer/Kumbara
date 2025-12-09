4f30154f813d1903997d885b470ece2299b0d244
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-12-09 17:35:47 +0300

    feat: implement conditional rendering of the auth screen based on a user authorized or not

--- navigation/RootStack.tsx
+++ navigation/RootStack.tsx
@@ -1,5 +1,5 @@
 // navigation/RootStack.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import TabNavigator from "./TabNavigator";
@@ -9,12 +9,29 @@ import { RootStackParamList } from "./NavigationTypes";
 import HomeScreen from "@/_tabs_/home/HomeScreen";
 import SettingsScreen from "@/_tabs_/settings/SettingsScreen";
 import AuthScreen from "@/screens/auth/AuthScreen";
+import { isAuthenticated } from "@/utils/auth";
+import { ActivityIndicator } from "react-native";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootStack = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const checkAuth = async () => {
+      const authenticated = await isAuthenticated();
+      setIsLoggedIn(authenticated);
+      setIsLoading(false);
+    };
+    checkAuth();
+  }, []);
+  if (isLoading) {
+    // Show loading screen while checking auth
+    return <ActivityIndicator />;
+  }
   return (
     <Stack.Navigator
+      initialRouteName={isLoggedIn ? "Home" : "AuthScreen"}
       screenOptions={{ headerShown: false, animation: "ios_from_right" }}
     >
       <Stack.Screen name="AuthScreen" component={AuthScreen} />
