81fe36c459a02def3fee92517369ae4fc888cecf
Tunahan Yelmer <tunahanyelmer@gmail.com>
2025-12-09 17:29:15 +0300

    feat: profile picture and the the given name of the  user succesfully retrieved from google

--- components/User/User.tsx
+++ components/User/User.tsx
@@ -2,26 +2,29 @@ import React, { FC } from "react";
 import { View, Text, Image, Dimensions } from "react-native";
 import { createUserStyles } from "./styles/User.styles";
 import { useTheme } from "@/context/theme/ThemeProvider";
+import { useDataLayerValue } from "@/context/state/StateProvider";
 import Notifications from "@/components/notifications/Notifications";
 
 const { width } = Dimensions.get("window");
 
 const User: FC = () => {
   const [theme] = useTheme();
-
+  const [{ User }] = useDataLayerValue();
+  const photo = User?.[0]?.photo;
+  const givenName = User?.[0]?.givenName;
   const styles = createUserStyles(theme, width);
   return (
     <View style={styles.userContainer}>
       <View style={styles.userIcon}>
         <View>
           <Image
-            source={{ uri: "https://picsum.photos/seed/picsum/200/300" }}
+            source={{ uri: photo ?? undefined }}
             style={styles.userImage}
           />
         </View>
         <View style={styles.userInfo}>
-          <Text style={styles.userLabel}>UserName</Text>
-          <Text style={styles.userWelcome}>Welcome Back!!</Text>
+          <Text style={styles.userLabel}>{givenName}</Text>
+          <Text style={styles.userWelcome}>Hoşgeldin !!</Text>
         </View>
       </View>
       <View>
