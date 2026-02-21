46b1c89c12c2ef7dbe700acab7c184e3af8fc6ed
Tunahan Yelmer <tunahanyelmer@gmail.com>
2026-02-21 17:14:22 +0300

    auth context now wraps whole app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootStack } from "@/navigation/RootStack";
+import { AuthProvider } from "./context/auth/AuthProvider";
 import { ThemeProvider } from "@/context/theme/ThemeProvider";
 import { StateProvider } from "@context/state/StateProvider";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -13,23 +14,25 @@ import Navbar from "./navigation/Navbar/Navbar";
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ThemeProvider>
-        <StateProvider>
-          <NavigationProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-              <SafeAreaView
-                style={{ flex: 1 }}
-                edges={["top", "left", "right", "bottom"]}
-              >
-                <NavigationContainer ref={navigationRef}>
-                  <RootStack />
-                  <Navbar />
-                </NavigationContainer>
-              </SafeAreaView>
-            </GestureHandlerRootView>
-          </NavigationProvider>
-        </StateProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <StateProvider>
+            <NavigationProvider>
+              <GestureHandlerRootView style={{ flex: 1 }}>
+                <SafeAreaView
+                  style={{ flex: 1 }}
+                  edges={["top", "left", "right", "bottom"]}
+                >
+                  <NavigationContainer ref={navigationRef}>
+                    <RootStack />
+                    <Navbar />
+                  </NavigationContainer>
+                </SafeAreaView>
+              </GestureHandlerRootView>
+            </NavigationProvider>
+          </StateProvider>
+        </ThemeProvider>
+      </AuthProvider>
     </SafeAreaProvider>
   );
 }
